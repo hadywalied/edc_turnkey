6f8d1e6c09c3e1f2f81c35b4c3ac24714124a5ba
Hady Walied <my.mepharo1320@gmail.com>
2021-08-30 16:53:44 +0200

    fixed bugs

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from src.module.smoketestanalyzer import SmokeTestAnalyzer
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    module = SmokeTestAnalyzer(solution='vved',
-                               env_file='/home/oalaa/maadi/VirtualEthernet_v11.4.0/b4233/vved1140.bash',
-                               examples_list=["CGMII"], compilation_type='Simulation',
-                               logging_dir='/home/oalaa/maadi/hwalied/tmp_logs',
-                               output_dir='/home/oalaa/maadi/hwalied/tmp_logs',
-                               hosts_file='/home/hwalied/tk/stamp_master/Utilities/ethernet-desgins-compiler/hosts',
-                               distribute=True,
-                               avb_list=[1], mode_of_operation="run", compiled_path="/home/oalaa/maadi/hwalied/tmp_logs",
-                               check_method_name="run_validation")
-    module.start()
+    smoke = SmokeTestAnalyzer(solution='vved',
+                              env_file='/home/oalaa/maadi/VirtualEthernet_v11.4.0/b4233/vved1140.bash',
+                              examples_list=["CGMII"], compilation_type='Simulation',
+                              logging_dir='/home/oalaa/maadi/hwalied/tmp_logs',
+                              output_dir='/home/oalaa/maadi/hwalied/tmp_logs',
+                              hosts_file='/home/hwalied/tk/stamp_master/Utilities/ethernet-desgins-compiler/hosts',
+                              distribute=True,
+                              avb_list=[1], mode_of_operation="run", compiled_path="/home/oalaa/maadi/hwalied/tmp_logs",
+                              check_method_name="run_validation")
+    smoke.start()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
